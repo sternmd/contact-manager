16a56bd9271fa9bb90cb88c51557c56885598aa8
Maxwell Stern <stern.maxwell@gmail.com>
2018-10-18 16:23:11 -0500

    testing GET on componentDidMount()

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,30 +1,39 @@
 import React, { Component } from 'react';
 
 class Test extends Component {
+  state = {
+    title: '',
+    body: ''
+  }
 componentDidMount() {
-  console.log('component did mount');
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(data => this.setState({
+    title: data.title
+  }))
 } // runs after it mounts
 
-componentWillMount() {
-    console.log('component will mount');
-} // runs before it mounts
-
-componentDidUpdate() {
-    console.log('component did update');
-} // runs if updates
-
-componentWillUpdate() {
-    console.log('component did update');
-} // runs before did update
+// componentWillMount() {
+//     console.log('component will mount');
+// } // runs before it mounts
+//
+// componentDidUpdate() {
+//     console.log('component did update');
+// } // runs if updates
+//
+// componentWillUpdate() {
+//     console.log('component did update');
+// } // runs before did update
 
 compoentWillReceiveProps(nextProps, nextState){
   console.log('test')
 } // whne component receives new properties, it will run
 
   render() {
+    const { title } = this.state;
     return (
       <div>
-        <h1>Test component</h1>
+        <h1>{title}</h1>
       </div>
     );
   }
