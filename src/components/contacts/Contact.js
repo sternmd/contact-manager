5358e012aefd65361cc19e898ac8f550a4f3ab68
Maxwell Stern <stern.maxwell@gmail.com>
2018-10-18 16:36:27 -0500

    DELETE request via axios

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './contact.css';
 import { Consumer } from '../../context';
+import axios from 'axios';
 
 class Contact extends Component {
   state = {
@@ -15,10 +16,8 @@ class Contact extends Component {
   };
 
   onDeleteClick = (id, dispatch) => {
-    dispatch({
-      type: 'DELETE_CONTACT',
-      payload: id
-    });
+    axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(res => dispatch({ type: 'DELETE_CONTACT', payload: id }));
   };
 
   render() {
