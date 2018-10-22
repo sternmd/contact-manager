fba8ac1ae10a0b5efb33a9cd64edc077f8467491
Maxwell Stern <stern.maxwell@gmail.com>
2018-10-22 17:31:11 -0500

    PUT Request & Update action

--- src/components/contacts/editContact.js
+++ src/components/contacts/editContact.js
@@ -52,6 +52,16 @@ class EditContact extends Component {
       return;
     }
 
+    const updateContact = {
+      name,
+      email,
+      phone
+    };
+
+    const  { id } = this.props.match.params;
+    const res = await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, updateContact);
+    dispatch({type: 'UPDATE_CONTACT', payload: res.data });
+    
     // Clear State
     this.setState({
       name: '',
